2f0c4ec5856265bbf3fa024cefaf33974205132d
ABIN VINOD <abinvinod@ABINs-mAc.local>
2024-01-26 23:27:00 +0530

    hash map method

--- TwoSum/Twosum-bestcase.cpp
+++ TwoSum/Twosum-bestcase.cpp
@@ -1,15 +1,13 @@
 
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 
 int main () {
     int nums[] = {20,10,40,60 , 30};
     int t = 50;
-    int siz;
-    siz = sizeof(nums)/ sizeof(nums[0]);
+     unordered_map <int ,int> newMap;
 
-    int size = siz - 1;
 
-    cout << size;
 }
