be0e482fe0cfd3deb13968b8bd304f4954326c5f
ABIN VINOD <abinbvinod@gmail.com>
2024-01-31 23:38:40 +0530

    best base two sum

--- TwoSum/Twosum-bestcase.cpp
+++ TwoSum/Twosum-bestcase.cpp
@@ -1,14 +1,26 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 
 int main () {
-    int nums[] = {20,10,40,60 , 30};
+    vector <int> nums = {20,10,40,60 , 30};
     int t = 50;
-     unordered_map <int ,int> newMap;
+     unordered_map <int ,int> num_count;
+//    int n = nums.size();
+//    int s = n - 1;
+   for (int i = 0; i < nums.size(); i++) {
+       int num = nums[i];
+       int check = t - num;
 
+       if(num_count.find(check) != num_count.end()){
+           cout << "Found :(" << check << "," << num << ")" << endl;
+       }
+       num_count[num]++;
+   }
+   return 0;
 
 }
 
